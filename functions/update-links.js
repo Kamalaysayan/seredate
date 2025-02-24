6caf206277f762b7ab6f38daa72c2a29ac482a35
Kamalaysayan <mmckamalaysayanclub@gmail.com>
2025-02-24 18:55:36 +0800

    sync 3

--- functions/update-links.js
+++ functions/update-links.js
@@ -1,14 +1,47 @@
+const fs = require('fs'); // Import the file system module
+
 exports.handler = async (event) => {
     try {
-        const data = [{ link: "https://www.example.com" }]; // Placeholder data for now
+        const method = event.httpMethod;
 
-        return {
-            statusCode: 200,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        };
+        if (method === 'GET') {
+            // Read data from data.json
+            const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            };
+        } else if (method === 'POST') {
+            // Add new link to data.json
+            const {
+                link
+            } = JSON.parse(event.body);
+            const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
+            data.push({
+                link
+            });
+            fs.writeFileSync('./data.json', JSON.stringify(data, null, 2));
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    link
+                })
+            };
+        } else {
+            return {
+                statusCode: 405, // Method Not Allowed
+                body: "Method Not Allowed"
+            };
+        }
     } catch (error) {
         console.error("Error in function:", error);
         return {
