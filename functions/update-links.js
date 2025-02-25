dcee6e17c960e3c194cf31d65b6fcc6f91cfdab7
Kamalaysayan <mmckamalaysayanclub@gmail.com>
2025-02-26 02:48:04 +0800

    delete button added

--- functions/update-links.js
+++ functions/update-links.js
@@ -1,28 +1,40 @@
-const fs = require('fs');
-const path = require('path');
+const { NetlifyDB } = require('@netlify/db')
+
+const db = new NetlifyDB({
+  // Your Netlify site ID is automatically detected in production.
+  // In development, you can provide it:
+  // siteId: 'YOUR_SITE_ID'
+})
 
 exports.handler = async (event) => {
     try {
         const method = event.httpMethod;
-        const data = require('./data.json'); // Load data.json using require
 
         if (method === 'GET') {
+            const links = await db.collection('links').list() // Use list() for all items
             return {
                 statusCode: 200,
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data)
+                body: JSON.stringify(links)
             };
         } else if (method === 'POST') {
             const { link } = JSON.parse(event.body);
 
-            data.push({ link });
-
-            fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(data, null, 2)); // Write back to data.json
-
+            const newLink = await db.collection('links').create({
+                link: link
+            })
             return {
                 statusCode: 200,
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ link })
+                body: JSON.stringify(newLink)
+            };
+        } else if (method === 'DELETE') {
+            const { id } = JSON.parse(event.body); // Get the ID to delete
+            await db.collection('links').delete(id); // Delete the link by ID
+            return {
+                statusCode: 200,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ message: "Link deleted successfully" })
             };
         } else {
             return {
